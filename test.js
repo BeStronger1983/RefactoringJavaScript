ea1388edb064ad18d59aabdbdd870965a0c3202d
ponpon <wizardsoloist1983@gmail.com>
2021-06-20 16:44:14 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,89 @@
+const deepEqual = require('deep-equal');
+var wish = require('wish');
+
+function checkHand(hand) {
+    if(isPair(hand)){
+        return 'pair';
+    }else{
+        return 'three of a kind';
+    }
+}
+
+function isPair(hand){
+    return multiplesIn(hand) === 2;
+}
+
+function multiplesIn(hand) {
+    return highestCount(valuesFromHand(hand));
+};
+
+function highestCount(values) {
+    var counts = {}
+    
+    values.forEach(function(value, index){
+        counts[value] = 0;
+        if(value == values[0]){
+            counts[value] = counts[value] + 1;
+        };
+        if(value == values[1]){
+            counts[value] = counts[value] + 1;
+        };
+        if(value == values[2]){
+            counts[value] = counts[value] + 1;
+        };
+        if(value == values[3]){
+            counts[value] = counts[value] + 1;
+        };
+        if(value == values[4]){
+            counts[value] = counts[value] + 1;
+        };
+    });
+
+    var totalCounts = Object.keys(counts).map(function(key){
+        return counts[key];
+    });
+
+    return totalCounts.sort(function(a,b){return b-a})[0];
+};
+
+describe('highestCount()', function(){
+    it('返回陣列中同點數手牌的最大張數', function(){
+        var result = highestCount(['2','4','4','4','2']);
+        wish(result === 3);
+    });
+});
+
+function valuesFromHand(hand) {
+    return hand.map(function(card){
+        return card.split('-')[0];
+    })
+};
+
+describe('valuesFromHand()', function(){
+    it('只從手牌中返回點數', function(){
+        var result = valuesFromHand(['2-H','3-C','4-D','5-H','2-C']);
+        wish(deepEqual(result,['2','3','4','5','2']));
+    });
+});
+
+describe('isPair()', function(){
+    it('找到一組對子', function(){
+        var result = isPair(['2-H','3-C','4-D','5-H','2-C']);
+        wish(result);
+    });
+});
+
+describe('checkHand()', function (){
+    it('處理對子', function(){
+        var result = checkHand(['2-H','3-C','4-D','5-H','2-C']);
+        wish(result === 'pair');
+
+        var anotherResult = checkHand(['3-H','3-C','4-D','5-H','2-C']);
+        wish(anotherResult === 'pair');
+    });
+
+    it('處理三條', function(){
+        var result = checkHand(['3-H','3-C','3-D','5-H','2-H']);
+        wish(result === 'three of a kind');
+    });
+});
