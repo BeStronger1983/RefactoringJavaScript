89cb82dbab9e8d48f93af4f8e76f93e5229d0ec7
ponpon <wizardsoloist1983@gmail.com>
2021-06-20 17:20:49 +0800

    新增四條、高牌、同花的判斷

--- test.js
+++ test.js
@@ -4,13 +4,31 @@ var wish = require('wish');
 function checkHand(hand) {
     if(isPair(hand)){
         return 'pair';
-    }else{
+    }else if(isTriple(hand)){
         return 'three of a kind';
+    }else if(isQuadruple(hand)){
+        return 'four of a kind';
+    }else if(isFlush(hand)){
+        return 'flush';
+    }else{
+        return 'high card';
     }
 }
 
 function isPair(hand){
     return multiplesIn(hand) === 2;
+}
+
+function isTriple(hand){
+    return multiplesIn(hand) === 3;
+}
+
+function isQuadruple(hand){
+    return multiplesIn(hand) === 4;
+}
+
+function isFlush(hand) {
+    return allTheSameSuit(suitsFor(hand));
 }
 
 function multiplesIn(hand) {
@@ -46,18 +64,36 @@ function highestCount(values) {
     return totalCounts.sort(function(a,b){return b-a})[0];
 };
 
+function valuesFromHand(hand) {
+    return hand.map(function(card){
+        return card.split('-')[0];
+    })
+};
+
+function allTheSameSuit(suits) {
+    var toReturn = true;
+
+    suits.forEach(function(suit){
+        if(suit !== suits[0]){
+            toReturn = false;
+        }
+    })
+
+    return toReturn;
+}
+
+function suitsFor(hand) {
+    return hand.map(function(card){
+        return card.split('-')[1];
+    });
+}
+
 describe('highestCount()', function(){
     it('返回陣列中同點數手牌的最大張數', function(){
         var result = highestCount(['2','4','4','4','2']);
         wish(result === 3);
     });
 });
-
-function valuesFromHand(hand) {
-    return hand.map(function(card){
-        return card.split('-')[0];
-    })
-};
 
 describe('valuesFromHand()', function(){
     it('只從手牌中返回點數', function(){
@@ -85,5 +121,27 @@ describe('checkHand()', function (){
     it('處理三條', function(){
         var result = checkHand(['3-H','3-C','3-D','5-H','2-H']);
         wish(result === 'three of a kind');
+    });
+
+    it('處理四條', function(){
+        var result = checkHand(['3-H','3-C','3-D','3-S','2-H']);
+        wish(result === 'four of a kind');
+    });
+
+    it('處理高牌', function(){
+        var result = checkHand(['2-H','5-C','9-D','7-S','3-H']);
+        wish(result === 'high card');
+    });
+
+    it('同花', function(){
+        var result = checkHand(['2-H','5-H','9-H','7-H','3-H']);
+        wish(result === 'flush');
+    });
+});
+
+describe('allTheSameSuit()', function(){
+    it('如果所有元素皆相同，回報 true', function(){
+        var result = allTheSameSuit(['D','D','D','D','D']);
+        wish(result);
     });
 });
