5411c3ed2d5ba4f551f74c035f1deada270d195f
ponpon <wizardsoloist1983@gmail.com>
2021-07-01 15:31:59 +0800

    連接字串

--- ch6/nb.js
+++ ch6/nb.js
@@ -125,4 +125,8 @@ function fileName(){
 
 console.log(`Welcome to ${fileName()}!`);
 
-var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+var text = "Lorem ipsum dolor sit amet, " +
+"consectetur adipiscing elit, sed do eiusmod " +
+"tempor incididunt ut labore et dolore magna aliqua. " +
+"Ut enim ad minim veniam, quis nostrud exercitation " +
+"ullamco laboris nisi ut aliquip ex ea commodo consequat."
