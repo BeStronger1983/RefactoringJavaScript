e14a4c2c5ec205e09a9ee22c17d3649b95218275
ponpon <wizardsoloist1983@gmail.com>
2021-07-01 17:50:18 +0800

    一個布林值陣列

--- ch7/nb.js
+++ ch7/nb.js
@@ -115,3 +115,14 @@ function classify(chords) {
 
 classify(["d", "g", "e", "dm"]);
 classify(["f#m7", "a", "dadd9", "dmaj7", "bm", "bm7", "d", "f#m"]);
+
+var states = [
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true
+];
