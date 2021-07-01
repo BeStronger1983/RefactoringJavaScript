28cde05a0154d997ad78bdc53d98969ef95dc855
ponpon <wizardsoloist1983@gmail.com>
2021-07-01 16:01:52 +0800

    用 map 創造一個被乘以兩倍的新陣列

--- ch6/nb.js
+++ ch6/nb.js
@@ -139,3 +139,8 @@ var newArray = [];
     newArray.push(element * 2);
 });
 console.log(newArray);
+
+var newArray = [2, 3, 4].map((element) => {
+    return element * 2;
+});
+console.log(newArray);
