9c0f31a45d93a9ff512666448cd0ec0e3e4357db
ponpon <wizardsoloist1983@gmail.com>
2021-07-04 15:01:18 +0800

    使用位元欄 bit fields 替代陣列

--- ch7/nb.js
+++ ch7/nb.js
@@ -116,15 +116,23 @@ function classify(chords) {
 classify(["d", "g", "e", "dm"]);
 classify(["f#m7", "a", "dadd9", "dmaj7", "bm", "bm7", "d", "f#m"]);
 
-var states = [
-    true,
-    true,
-    true,
-    true,
-    true,
-    true,
-    false,
-    true
-];
+var states = [true, true, true, true, true, true, false, true];
+
+function stateIsOk(state) {
+    return (
+        state[0] &&
+        state[1] &&
+        state[2] &&
+        state[3] &&
+        state[4] &&
+        state[5] &&
+        !state[6] &&
+        state[7]
+    );
+}
 
 var states = 0b11111101;
+
+function stateIsOk(state) {
+    return state === 0b11111101;
+}
