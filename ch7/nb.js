fac899af22daaad49d1c725920c5014022959646
ponpon <wizardsoloist1983@gmail.com>
2021-07-10 22:35:28 +0800

    重構 classifier 的 classify 函式

--- ch7/nb.js
+++ ch7/nb.js
@@ -93,16 +93,24 @@ function classify(chords) {
         _probabilities,
         difficulty
     ) {
-        let first = classifier.labelProbabilities.get(difficulty) + smoothing;
+        const likelihoods = [
+            classifier.labelProbabilities.get(difficulty) + smoothing,
+        ];
+
         chords.forEach(function (chord) {
             const probabilityOfChordInLabel = classifier.probabilityOfChordsInLabels.get(
                 difficulty
             )[chord];
             if (probabilityOfChordInLabel) {
-                first = first * (probabilityOfChordInLabel + smoothing);
+                likelihoods.push(probabilityOfChordInLabel + smoothing);
             }
         });
-        classified.set(difficulty, first);
+
+        const totalLikelihood = likelihoods.reduce(function (total, index) {
+            return total * index;
+        });
+
+        classified.set(difficulty, totalLikelihood);
     });
     return classified;
 }
