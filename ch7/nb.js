41a6c365111620763cedc225414bdc07f074837c
ponpon <wizardsoloist1983@gmail.com>
2021-07-14 12:07:33 +0800

    Classifier 新增 addSong 函式，並使用 ... 語法

--- ch7/nb.js
+++ ch7/nb.js
@@ -63,6 +63,11 @@ class Classifier {
             })
         );
     }
+    // 其餘參數 rest parameter：代表接收參數，把傳進來的參數包裝成陣列
+    addSong(...songParams) {
+        // 展開參數 spread operator：代表傳送參數，把陣列展開成獨立的參數給呼叫它的函式
+        this.songList.addSong(...songParams);
+    }
 }
 
 const wish = require("wish");
@@ -70,15 +75,15 @@ const wish = require("wish");
 describe("the file", () => {
     const classifier = new Classifier();
 
-    classifier.songList.addSong("imagine", ["c", "cmaj7", "f", "am", "dm", "g", "e7"], 0);
-    classifier.songList.addSong("someWhereOverTheRainbow", ["c", "em", "f", "g", "am"], 0);
-    classifier.songList.addSong("tooManyCooks", ["c", "g", "f"], 0);
-    classifier.songList.addSong("iWillFollowYouIntoTheDark", ["f", "dm", "bb", "c", "a", "bbm"], 1);
-    classifier.songList.addSong("babyOneMoreTime", ["cm", "g", "bb", "eb", "fm", "ab"], 1);
-    classifier.songList.addSong("creep", ["g", "gsus4", "b", "bsus4", "c", "cmsus4", "cm6"], 1);
-    classifier.songList.addSong("paperBag", ["bm7", "e", "c", "g", "b7", "f", "em", "a", "cmaj7", "em7", "a7", "f7", "b"], 2);
-    classifier.songList.addSong("toxic", ["cm", "eb", "g", "cdim", "eb7", "d7", "db7", "ab", "gmaj7", "g7"], 2);
-    classifier.songList.addSong("bulletproof", ["d#m", "g#", "b", "f#", "g#m", "c#"], 2);
+    classifier.addSong("imagine", ["c", "cmaj7", "f", "am", "dm", "g", "e7"], 0);
+    classifier.addSong("someWhereOverTheRainbow", ["c", "em", "f", "g", "am"], 0);
+    classifier.addSong("tooManyCooks", ["c", "g", "f"], 0);
+    classifier.addSong("iWillFollowYouIntoTheDark", ["f", "dm", "bb", "c", "a", "bbm"], 1);
+    classifier.addSong("babyOneMoreTime", ["cm", "g", "bb", "eb", "fm", "ab"], 1);
+    classifier.addSong("creep", ["g", "gsus4", "b", "bsus4", "c", "cmsus4", "cm6"], 1);
+    classifier.addSong("paperBag", ["bm7", "e", "c", "g", "b7", "f", "em", "a", "cmaj7", "em7", "a7", "f7", "b"], 2);
+    classifier.addSong("toxic", ["cm", "eb", "g", "cdim", "eb7", "d7", "db7", "ab", "gmaj7", "g7"], 2);
+    classifier.addSong("bulletproof", ["d#m", "g#", "b", "f#", "g#m", "c#"], 2);
 
     classifier.trainAll();
 
