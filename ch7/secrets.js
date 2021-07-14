bac38246c3a655a64150a62800d0063ef219e3c6
ponpon <wizardsoloist1983@gmail.com>
2021-07-14 11:28:47 +0800

    更簡潔的工廠函式

--- ch7/secrets.js
+++ ch7/secrets.js
@@ -13,7 +13,7 @@ const Secret = function () {
 const s = new Secret();
 console.log(s.normalInfo); // 'this is normal'
 console.log(s.secret); // undefined
-console.log(s.secretFunction()); // error
+// console.log(s.secretFunction()); // error
 console.log(s.notSecret()); // 'sekrit'
 console.log(s.totallyNotSecret); // undefined
 console.log(totallyNotSecret); // I'm defined in the global scope
@@ -35,7 +35,27 @@ var secretTemplate = (function(){
 const se = Object.create(secretTemplate);
 console.log(se.normalInfo); // 'this is normal'
 console.log(se.secret); // undefined
-console.log(se.secretFunction()); // error
+// console.log(se.secretFunction()); // error
 console.log(se.notSecret()); // 'sekrit'
 console.log(se.totallyNotSecret); // undefined
+console.log(totallyNotSecret); // "I'm defined in the global scope"
+
+// Factory Function (concise)
+var secretTemplate = (function(){
+  const secret = 'sekrit';
+  const secretFunction = function(){
+    return secret;
+  };
+  totallyNotSecret = "I'm defined in the global scope";
+  return {normalInfo: 'this is normal',
+          notSecret(){
+            return secret;
+          }}
+})();
+const sec = Object.create(secretTemplate);
+console.log(sec.normalInfo); // 'this is normal'
+console.log(sec.secret); // undefined
+// console.log(sec.secretFunction()); // error
+console.log(sec.notSecret()); // 'sekrit'
+console.log(sec.totallyNotSecret); // undefined
 console.log(totallyNotSecret); // "I'm defined in the global scope"
