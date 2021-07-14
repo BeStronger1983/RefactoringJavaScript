a3feec78d5589a3d2a313e35f6706c29dc12dfa8
ponpon <wizardsoloist1983@gmail.com>
2021-07-14 11:21:55 +0800

    工廠函式 Factory Function

--- ch7/secrets.js
+++ ch7/secrets.js
@@ -17,3 +17,25 @@ console.log(s.secretFunction()); // error
 console.log(s.notSecret()); // 'sekrit'
 console.log(s.totallyNotSecret); // undefined
 console.log(totallyNotSecret); // I'm defined in the global scope
+
+// Factory Function
+var secretTemplate = (function(){
+  var obj = {};
+  obj.normalInfo = 'this is normal';
+  const secret = 'sekrit';
+  const secretFunction = function(){
+    return secret;
+  };
+  obj.notSecret = function(){
+    return secret;
+  };
+  totallyNotSecret = "I'm defined in the global scope";
+  return obj;
+})();
+const se = Object.create(secretTemplate);
+console.log(se.normalInfo); // 'this is normal'
+console.log(se.secret); // undefined
+console.log(se.secretFunction()); // error
+console.log(se.notSecret()); // 'sekrit'
+console.log(se.totallyNotSecret); // undefined
+console.log(totallyNotSecret); // "I'm defined in the global scope"
