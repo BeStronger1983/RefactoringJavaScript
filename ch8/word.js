903d7e926dd0319a6fe76f818ba692c638cd53fc
ponpon <wizardsoloist1983@gmail.com>
2021-07-15 11:25:40 +0800

    建立測試環境

--- ch8/word.js
+++ ch8/word.js
@@ -31,3 +31,30 @@ console.log(japaneseWord.lookUp());
 console.log(englishWord.word);
 console.log(englishWord.count());
 console.log(englishWord.lookUp());
+
+const wish = require("wish");
+const deepEqual = require("deep-equal");
+
+// 介面測試
+wish(japaneseWord.word === "犬");
+wish(japaneseWord.lookUp() === "http://jisho.org/search/犬");
+wish(japaneseWord.count() === 1);
+
+wish(englishWord.word === "dog");
+wish(englishWord.lookUp() === "https://en.wiktionary.org/wiki/dog");
+wish(englishWord.count() === 3);
+
+// 內部測試
+wish(typeof japaneseWord === "object");
+wish(typeof JapaneseWord === "function");
+wish(japaneseWord instanceof JapaneseWord);
+wish(japaneseWord instanceof Word);
+wish(!(JapaneseWord instanceof Word));
+
+wish(japaneseWord.constructor === JapaneseWord);
+wish(Object.getPrototypeOf(JapaneseWord) === Word);
+
+// 模糊點
+wish(deepEqual(Object.getPrototypeOf(japaneseWord), {}));
+console.log(Object.getPrototypeOf(japaneseWord));
+// 回報 JapaneseWord {}
