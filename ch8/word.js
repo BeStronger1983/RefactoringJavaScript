6d6221fb3fa477d949123b886d56d8ca818d8ca5
ponpon <wizardsoloist1983@gmail.com>
2021-07-29 00:06:28 +0800

    改用物件字面值

--- ch8/word.js
+++ ch8/word.js
@@ -1,37 +1,23 @@
-function Word(word, language, lookUpUrl) {
-    this.word = word;
-    this.language = language;
-    this.lookUpUrl = lookUpUrl;
-    this.count = function () {
+// 物件字面值
+const word = {
+    count() {
         return this.word.length;
-    };
-    this.lookUp = function () {
+    },
+    lookUp() {
         return this.lookUpUrl + this.word;
-    };
-}
+    },
+};
 
-function EnglishWord(word) {
-    Word.call(this, word, "English", "https://en.wiktionary.org/wiki/");
-}
+const englishWord = Object.create(word);
+englishWord.word = "dog";
+englishWord.language = "English";
+englishWord.lookUpUrl = "https://en.wiktionary.org/wiki/";
 
-function JapaneseWord(word) {
-    Word.call(this, word, "Japanese", "http://jisho.org/search/");
-}
+const japaneseWord = Object.create(word);
+japaneseWord.word = "犬";
+japaneseWord.language = "Japanese";
+japaneseWord.lookUpUrl = "http://jisho.org/search/";
 
-// 如果只有第一行
-JapaneseWord.prototype = Object.create(Word.prototype);
-// JapaneseWord.prototype.constructor = JapaneseWord;
-
-// EnglishWord.prototype = Object.create(Word.prototype);
-// EnglishWord.prototype.constructor = EnglishWord;
-
-// Word.prototype.reportLanguage = function () {
-//     return `The language is: ${this.language}`;
-// };
-
-const japaneseWord = new JapaneseWord("犬");
-// console.log(japaneseWord.reportLanguage());
-const englishWord = new EnglishWord("dog");
 console.log(japaneseWord.word);
 console.log(japaneseWord.count());
 console.log(japaneseWord.lookUp());
@@ -53,15 +39,15 @@ wish(englishWord.count() === 3);
 
 // 內部測試
 wish(typeof japaneseWord === "object");
-wish(typeof JapaneseWord === "function");
-wish(japaneseWord instanceof JapaneseWord);
-wish(japaneseWord instanceof Word);
-wish(!(JapaneseWord instanceof Word));
+// wish(typeof JapaneseWord === "function");
+// wish(japaneseWord instanceof JapaneseWord);
+// wish(japaneseWord instanceof Word);
+// wish(!(JapaneseWord instanceof Word));
 
 // 這個測試也過不了
 // wish(japaneseWord.constructor === JapaneseWord);
 // wish(Object.getPrototypeOf(JapaneseWord) === Word);
-console.log(Object.getPrototypeOf(JapaneseWord));
+// console.log(Object.getPrototypeOf(JapaneseWord));
 
 // 模糊點
 // wish(deepEqual(Object.getPrototypeOf(japaneseWord), {}));
