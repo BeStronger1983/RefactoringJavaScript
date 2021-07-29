7e74f59666fddd323da7b101abe8fbf963d63b5e
ponpon <wizardsoloist1983@gmail.com>
2021-07-29 23:59:13 +0800

    用 mixin 達成多重繼承

--- ch8/multiple_inheritance.js
+++ ch8/multiple_inheritance.js
@@ -1,31 +1,29 @@
-class Barky {
+const barky = {
     bark() {
         console.log("woof woof");
-    }
-}
+    },
+};
 
-class Bitey {
+const bitey = {
     bark() {
         console.log("grrr");
-    }
+    },
     bite() {
         console.log("real bite");
-    }
-}
+    },
+};
 
-class Animal {
+const animal = {
     beFluffy() {
         console.log("fluffy");
-    }
+    },
     bite() {
         console.log("normal bite");
-    }
-}
+    },
+};
 
-// 在 JavaScript 中，這種寫法只會讓 Dog 繼承 Bitey
-class Dog extends (Animal, Barky, Bitey) {}
-
-const dog = new Dog();
-dog.bark();
-dog.bite();
-dog.beFluffy(); // TypeError: dog.beFluffy is not a function
+// 用 mixin 達成多重繼承
+const myPet = Object.assign(Object.create(animal), barky, bitey);
+myPet.bark();
+myPet.bite();
+myPet.beFluffy();
