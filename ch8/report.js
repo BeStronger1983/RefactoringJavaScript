b6f50afdd6a9cd18751e21527f62ee1f61615923
ponpon <wizardsoloist1983@gmail.com>
2021-08-02 16:01:43 +0800

    刪除 ClientReport 跟 SpecificClientReport，並整理程式碼

--- ch8/report.js
+++ ch8/report.js
@@ -17,31 +17,7 @@ class GenericReport extends Report {
     }
 }
 
-class ClientReport extends GenericReport {
-    constructor(params) {
-        super(params);
-        this.params = params;
-    }
-    printReport(params) {
-        return super.printReport(Object.assign(this.params, params));
-    }
-}
-
-class SpecificClientReport extends ClientReport {
-    constructor(params) {
-        super(params);
-        this.params = params;
-    }
-    printReport(params) {
-        return super.printReport(Object.assign(this.params, params));
-    }
-}
-
-// 改為 GenericReport
-const report = new GenericReport({ whatever: "we want", to: "add" });
-console.log(report.printReport({ extra: "params" }), true);
-
 const wish = require("wish");
 const deepEqual = require("deep-equal");
+const report = new GenericReport({ whatever: "we want", to: "add" });
 wish(deepEqual(report.printReport({ extra: "params" }), { whatever: "we want", to: "add", extra: "params" }));
-// 測試通過，表示可以刪除 ClientReport 跟 SpecificClientReport
