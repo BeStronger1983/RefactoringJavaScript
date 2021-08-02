739345b78e086076ff17434b31c8ad8d7dda4faf
ponpon <wizardsoloist1983@gmail.com>
2021-08-02 16:46:56 +0800

    刪除多餘的程式碼

--- ch8/agent_user_project.js
+++ ch8/agent_user_project.js
@@ -5,25 +5,8 @@ function coinToss() {
     return Math.random() > 0.5;
 }
 
-class Agent {
+class User {
     constructor(name, type) {
-        this.name = name;
-        this.type = type;
-    }
-
-    static makeProjetOrUser(agent) {
-        if (agent.type === "user") {
-            return Object.assign(Object.create(new User()), agent);
-        } else {
-            return Object.assign(Object.create(new Project()), agent);
-        }
-    }
-}
-
-class User extends Agent {
-    // 推入建構子到子類別
-    constructor(name, type) {
-        super();
         this.name = name;
         this.type = type;
     }
@@ -33,10 +16,8 @@ class User extends Agent {
     }
 }
 
-class Project extends Agent {
-    // 推入建構子到子類別
+class Project {
     constructor(name, type) {
-        super();
         this.name = name;
         this.type = type;
     }
@@ -54,13 +35,11 @@ if (coinToss()) {
     agent = new Project("name", "project");
 }
 
-const projectOrUser = Agent.makeProjetOrUser(agent);
-
 // 加入描述測試
 const wish = require("wish");
 
-if (projectOrUser.type === "user") {
-    wish(projectOrUser.sayName() === "my name is name");
+if (agent.type === "user") {
+    wish(agent.sayName() === "my name is name");
 } else {
-    wish(projectOrUser.sayTheName() === "the project name is name");
+    wish(agent.sayTheName() === "the project name is name");
 }
