6d95f5a18b9370deebb32c783c10562830fca11d
ponpon <wizardsoloist1983@gmail.com>
2021-08-04 16:19:14 +0800

    如果在建構子中加入一個用以儲存型別的變數，其實可以避免使用子類別

--- ch9/template_method.js
+++ ch9/template_method.js
@@ -1,24 +1,25 @@
+// 如果在建構子中加入一個用以儲存型別的變數，其實可以避免使用子類別
 class Person {
+    constructor(knowsBinary) {
+        this.knowsBinary = knowsBinary;
+    }
+
     log(number) {
         console.log(this.whatIs(number));
     }
-}
 
-class BinaryKnower extends Person {
     whatIs(number) {
-        return Number("0b" + number);
+        if (this.knowsBinary) {
+            return Number("0b" + number);
+        } else {
+            return number;
+        }
     }
 }
 
-class BinaryOblivious extends Person {
-    whatIs(number) {
-        return number;
-    }
-}
+const personOne = new Person(true);
+const personTwo = new Person(false);
 
-const personOne = new BinaryKnower();
-const personTwo = new BinaryOblivious();
-
-[personOne, personTwo].forEach((person) => {
+[(personOne, personTwo)].forEach((person) => {
     person.log(10);
 });
