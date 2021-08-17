6bf28a3db3005113d1736ea4056ccac50a40cb6e
ponpon <wizardsoloist1983@gmail.com>
2021-08-17 23:42:57 +0800

    null 物件模式

---
+++ ch9/null.js
@@ -0,0 +1,18 @@
+// null 物件模式
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class AnonymousPerson extends Person {
+    constructor() {
+        super();
+        this.name = null;
+    }
+}
+
+personOne = new Person("tony");
+personTwo = new AnonymousPerson("tony");
+console.log(personOne.name);
+console.log(personTwo.name);
