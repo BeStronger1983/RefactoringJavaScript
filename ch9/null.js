8494cc1951948c1bca495b685f6a7cccd5f7d480
ponpon <wizardsoloist1983@gmail.com>
2021-08-17 23:56:19 +0800

    假如想對這些值做運算，就會遇到麻煩

--- ch9/null.js
+++ ch9/null.js
@@ -16,3 +16,11 @@ personOne = new Person("tony");
 personTwo = new AnonymousPerson("tony");
 console.log(personOne.name);
 console.log(personTwo.name);
+
+function capitalize(str) {
+    // 假如想對這些值做運算，就會遇到麻煩
+    return str[0].toUpperCase() + str.substring(1);
+}
+
+console.log(capitalize(personOne.name));
+console.log(capitalize(personTwo.name)); // TypeError: Cannot read property '0' of null
