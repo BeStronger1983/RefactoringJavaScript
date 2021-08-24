3e0758a04078d9bbaf29d1399d557ba36325ae4c
ponpon <wizardsoloist1983@gmail.com>
2021-08-25 00:31:53 +0800

    加入 NameString 與 NullString 類別

--- ch9/null.js
+++ ch9/null.js
@@ -1,51 +1,45 @@
 // null 物件模式
 class Person {
     constructor(name) {
-        this.name = name;
+        this.name = new NameString(name);
     }
 }
 
 class AnonymousPerson extends Person {
     constructor() {
         super();
-        this.name = null; // 回到使用 null 的方法
+        this.name = new NullString();
+    }
+}
+
+// 加入 NameString 與 NullString 類別
+class NullString {
+    capitalize() {
+        return null;
+    }
+}
+
+class NameString extends String {
+    capitalize() {
+        return new NameString(this[0].toUpperCase() + this.substring(1));
+    }
+    tigerify() {
+        if (this === null) {
+            return null;
+        } else {
+            return new NameString(`${this}, the tiger`);
+        }
+    }
+    display() {
+        if (this === null) {
+            return "";
+        } else {
+            return this.toString();
+        }
     }
 }
 
 personOne = new Person("tony");
 personTwo = new AnonymousPerson("tony");
-console.log(personOne.name);
-console.log(personTwo.name);
-
-function capitalize(str) {
-    // capitalize 中要加入型別檢查
-    if (str == null) {
-        return null;
-    } else {
-        return str[0].toUpperCase() + str.substring(1);
-    }
-}
-
-console.log(capitalize(personOne.name));
-console.log(capitalize(personTwo.name)); // Cannot read property 'toUpperCase' of undefined
-
-// 試試其他的方法
-function tigerify(str) {
-    if (str === null) {
-        return null;
-    } else {
-        return `${str}, the tiger`;
-    }
-}
-
-// 執行了另一個 null 檢查，好讓我們顯示空白字串
-function display(str) {
-    if (str === null) {
-        return "";
-    } else {
-        return str;
-    }
-}
-
-console.log(display(tigerify(capitalize(personOne.name)))); // Tony, the tiger
-console.log(display(tigerify(capitalize(personTwo.name)))); // 顯示空白字串
+console.log(personOne.name.capitalize().tigerify().display()); // Tony, the tiger
+console.log(personTwo.name.capitalize()); // null
