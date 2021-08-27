f63956b06514cdc8477407fb1d1611d4cd868af9
ponpon <wizardsoloist1983@gmail.com>
2021-08-28 00:01:31 +0800

    如果我們希望用特別的特徵來裝飾 dog，可以使用一個將 dog 作為參數的工廠函式

--- ch9/decorator.js
+++ ch9/decorator.js
@@ -8,9 +8,21 @@ class Dog {
     }
 }
 
+// 如果我們希望用特別的特徵來裝飾 dog，可以使用一個將 dog 作為參數的工廠函式
+function Cute(dog) {
+    const cuteDog = Object.create(dog);
+    cuteDog.cost = dog.cost + 20;
+    return cuteDog;
+}
+
 const test = require("tape");
 
 test("base dog price", (assert) => {
     assert.equal(new Dog().displayPrice(), "The dog costs $50.");
+    assert.end();
+});
+
+test("cute dog price", (assert) => {
+    assert.equal(Cute(new Dog().displayPrice(), "The dog costs $70."));
     assert.end();
 });
