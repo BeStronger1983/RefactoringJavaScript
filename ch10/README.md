46f42b2cae3e66b81dd7c5d36a87d450872bf97f
ponpon <wizardsoloist1983@gmail.com>
2021-08-29 00:50:37 +0800

    setTimeout 的順序不可靠

--- ch10/README.md
+++ ch10/README.md
@@ -27,6 +27,8 @@
 
 當 timeout 設定為 0 秒時，程式碼會被立刻執行嗎？會比同步的程式碼更快執行嗎？請看看[這個例子](./chicken_or_egg.js)。
 
+再看看[這個例子](./chickens_or_egg.js)，蛋永遠最快，但雞不一定，Chrome 跟 FireFox 的順序不同，但每次的結果一致。node 的執行結果不是每次都一樣。
+
 <h2 id="fix">修復「毀滅金字塔(pyramid of doom)」</h2>
 
 <h2 id="test">測試異步程式碼</h2>
---
+++ ch10/chickens_or_egg.js
@@ -0,0 +1,25 @@
+setTimeout(() => {
+    console.log("chicken");
+}, 2);
+setTimeout(() => {
+    console.log("chicken 2");
+}, 0);
+setTimeout(() => {
+    console.log("chicken 3");
+}, 1);
+setTimeout(() => {
+    console.log("chicken 4");
+}, 1);
+setTimeout(() => {
+    console.log("chicken 5");
+}, 1);
+setTimeout(() => {
+    console.log("chicken 6");
+}, 1);
+setTimeout(() => {
+    console.log("chicken 7");
+}, 0);
+setTimeout(() => {
+    console.log("chicken 8");
+}, 2);
+console.log("egg");
