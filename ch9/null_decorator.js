ed45b317150e85a437153fb9f8ba41168bf70916
ponpon <wizardsoloist1983@gmail.com>
2021-08-28 13:30:21 +0800

    雖然這種做法可行，但如果測試 AnonymousPerson，處理 null 的檢查的時候會出問題

--- ch9/null_decorator.js
+++ ch9/null_decorator.js
@@ -31,3 +31,13 @@ test("Displaying a person", (assert) => {
     assert.equal(personOne.name.capitalize().tigerify().display(), "Tony, the tiger");
     assert.end();
 });
+
+// 雖然這種做法可行，但如果測試 AnonymousPerson，處理 null 的檢查的時候會出問題
+test("Displaying an anonymous person", (assert) => {
+    const personTwo = new AnonymousPerson("tony");
+    console.log(personTwo.name);
+    assert.equal(personTwo.name.capitalize().tigerify().display(), "");
+    assert.end();
+});
+
+// Uncaught TypeError: Cannot read property 'capitalize' of null
