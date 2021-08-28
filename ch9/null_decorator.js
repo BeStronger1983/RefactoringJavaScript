ed1120cb46a9dfdb847f24c458218cba6c1c9616
ponpon <wizardsoloist1983@gmail.com>
2021-08-28 13:35:13 +0800

    改變 AnonymousPerson 並加入 NullString，會修改到原本的程式碼，容易產生問題

--- ch9/null_decorator.js
+++ ch9/null_decorator.js
@@ -5,10 +5,11 @@ class Person {
     }
 }
 
+// 改變 AnonymousPerson 並加入 NullString
 class AnonymousPerson extends Person {
     constructor() {
         super();
-        this.name = null;
+        this.name = new NullString(); // 修改到原本的程式碼，容易產生問題
     }
 }
 
@@ -24,6 +25,18 @@ class NameString extends String {
     }
 }
 
+class NullString {
+    capitalize() {
+        return this;
+    }
+    tigerify() {
+        return this;
+    }
+    display() {
+        return "";
+    }
+}
+
 const test = require("tape");
 
 test("Displaying a person", (assert) => {
@@ -32,12 +45,8 @@ test("Displaying a person", (assert) => {
     assert.end();
 });
 
-// 雖然這種做法可行，但如果測試 AnonymousPerson，處理 null 的檢查的時候會出問題
 test("Displaying an anonymous person", (assert) => {
     const personTwo = new AnonymousPerson("tony");
-    console.log(personTwo.name);
     assert.equal(personTwo.name.capitalize().tigerify().display(), "");
     assert.end();
 });
-
-// Uncaught TypeError: Cannot read property 'capitalize' of null
