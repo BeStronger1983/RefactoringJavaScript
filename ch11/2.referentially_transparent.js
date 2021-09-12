73d00f53fcfd342bf0c84f414388c3e1ec690bd5
ponpon <wizardsoloist1983@gmail.com>
2021-09-12 16:52:48 +0800

    函式依賴非顯示參數，變為非純函式。修改 lookUpTable 為顯式參數

--- ch11/2.referentially_transparent.js
+++ ch11/2.referentially_transparent.js
@@ -32,3 +32,28 @@ function memoizedFactorial(number) {
 console.log(memoizedFactorial(10)); // 計算十次
 console.log(memoizedFactorial(10)); // 快取一次
 console.log(memoizedFactorial(11)); // 計算一次並快取
+
+// 函式依賴非顯示參數，變為非純函式。修改 lookUpTable 為顯式參數
+function memoizedFactorial(number, lookUpTable = {}) {
+    if (number in lookUpTable) {
+        console.log("快取");
+        return lookUpTable[number];
+    } else {
+        console.log("計算");
+        var reduceValue;
+        if (number < 2) {
+            reduceValue = 1;
+        } else {
+            reduceValue = number * memoizedFactorial(number - 1, lookUpTable)["result"];
+        }
+        lookUpTable[number] = reduceValue;
+        return { result: reduceValue, lookUpTable: lookUpTable };
+    }
+}
+
+console.log(memoizedFactorial(10)["result"]);
+console.log(memoizedFactorial(10)["result"]); // 沒有取到快取
+
+const resultAndLookup = memoizedFactorial(10);
+console.log(resultAndLookup.result);
+console.log(memoizedFactorial(10, resultAndLookup.lookUpTable)); // 有取到快取
