9f6c7c996cb3bd769aad7d379d2868d4c12e3d80
ponpon <wizardsoloist1983@gmail.com>
2021-09-15 13:03:30 +0800

    在條件測試中避免重新賦值

--- ch11/3.do_not_assign_value_again.js
+++ ch11/3.do_not_assign_value_again.js
@@ -1,3 +1,4 @@
+// 重新賦值
 let x = 1;
 x = x + 1;
 console.log(x);
@@ -5,3 +6,47 @@ console.log(x);
 // 避免重新賦值
 const y = 2;
 console.log(y);
+
+// 在條件測試中重新賦值
+function func(x) {
+    if (x >= 2) {
+        x = x + 7;
+    }
+
+    return x;
+}
+
+console.log(func(1));
+console.log(func(2));
+
+// 在條件測試中避免重新賦值
+function func2(x) {
+    if (x >= 2) {
+        return x + 7;
+    } else {
+        return x;
+    }
+}
+
+console.log(func2(1));
+console.log(func2(2));
+
+// 或
+function func3(x) {
+    return x >= 2 ? x + 7 : x;
+}
+
+console.log(func3(1));
+console.log(func3(2));
+
+// 條件內部的重複賦值
+function func4(x) {
+    if ((x = x + 7) >= 9) {
+        return x;
+    } else {
+        return x + 5;
+    }
+}
+
+console.log(func4(1));
+console.log(func4(2));
