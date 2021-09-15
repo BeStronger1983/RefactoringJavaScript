75e9886fe4184bba67c05698b1f37854c7b269f4
ponpon <wizardsoloist1983@gmail.com>
2021-09-15 13:13:58 +0800

    移除 console.log

--- ch11/3.do_not_assign_value_again.js
+++ ch11/3.do_not_assign_value_again.js
@@ -1,11 +1,9 @@
 // 重新賦值
 let x = 1;
 x = x + 1;
-console.log(x);
 
 // 避免重新賦值
 const y = 2;
-console.log(y);
 
 // 在條件測試中重新賦值
 function func(x) {
@@ -16,9 +14,6 @@ function func(x) {
     return x;
 }
 
-console.log(func(1));
-console.log(func(2));
-
 // 在條件測試中避免重新賦值
 function func2(x) {
     if (x >= 2) {
@@ -28,16 +23,10 @@ function func2(x) {
     }
 }
 
-console.log(func2(1));
-console.log(func2(2));
-
 // 或
 function func3(x) {
     return x >= 2 ? x + 7 : x;
 }
-
-console.log(func3(1));
-console.log(func3(2));
 
 // 條件內部的重複賦值
 function func4(x) {
@@ -47,9 +36,6 @@ function func4(x) {
         return x;
     }
 }
-
-console.log(func4(1));
-console.log(func4(2));
 
 // 先讓所有變數都不被改動
 function func5(x) {
